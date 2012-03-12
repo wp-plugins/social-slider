c23d4b02534a8447a65deb02a6ac301b603c075f
LukaszWiecek <LukaszWiecek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-12 12:48:06 +0000

    git-svn-id: https://plugins.svn.wordpress.org/social-slider/trunk@517906 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- social-slider.php
+++ social-slider.php
@@ -3,13 +3,13 @@
 Plugin Name: Social Slider
 Plugin URI: http://xn--wicek-k0a.pl/projekty/social-slider
 Description: This plugin adds links to your social networking sites' profiles in a box floating at the left side of the screen.
-Version: 7.1.1
+Version: 7.1.2
 Author: Łukasz Więcek
 Author URI: http://mydiy.pl/
 */
 
 $socialslider			= "social-slider";
-$socialslider_wersja	= "7.1.1";
+$socialslider_wersja	= "7.1.2";
 $socialslider_baza		= str_replace("https://", "http://", get_bloginfo('wpurl'));
 $socialslider_katalog	= WP_PLUGIN_URL .'/'.$socialslider;
 
@@ -1242,6 +1242,7 @@ if(get_option('socialslider_mobile')=="nie" || !get_option('socialslider_mobile'
 		"mini",				// Opera Mini Experimental
 		"Skyfire",			// Skyfire
 		"Nokia",			// Nokia Phones
+		"Android3",			// New Android Phones
 		);
 
 	asort($useragents);
