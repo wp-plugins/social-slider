36faf2425a8c9cee7a1af9e6be6b7789d050d567
LukaszWiecek <LukaszWiecek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-21 09:10:25 +0000

    git-svn-id: https://plugins.svn.wordpress.org/social-slider/trunk@546935 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- social-slider.php
+++ social-slider.php
@@ -3,13 +3,13 @@
 Plugin Name: Social Slider
 Plugin URI: http://xn--wicek-k0a.pl/projekty/social-slider
 Description: This plugin adds links to your social networking sites' profiles in a box floating at the left side of the screen.
-Version: 7.2.2
+Version: 7.2.3
 Author: Łukasz Więcek
 Author URI: http://majsterkowo.pl/
 */
 
 $socialslider			= "social-slider";
-$socialslider_wersja	= "7.2.2";
+$socialslider_wersja	= "7.2.3";
 $socialslider_baza		= str_replace("https://", "http://", get_bloginfo('wpurl'));
 $socialslider_katalog	= WP_PLUGIN_URL .'/'.$socialslider;
 
@@ -1232,6 +1232,7 @@ if(get_option('socialslider_mobile')=="nie" || !get_option('socialslider_mobile'
 		"blackberry9530",	// Storm
 		"blackberry9520",	// Storm v2
 		"blackberry9550",	// Storm v2
+		"blackberry9570",	// Storm v2
 		"blackberry9800",	// Torch
         "blackberry9900",	// Torch
 		"webOS",			// Palm Pre Experimental
