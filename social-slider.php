22da65e7f947dd1d9fa959834617ea1a3470bfc1
LukaszWiecek <LukaszWiecek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-26 19:09:29 +0000

    I added the user agent on Android 4.2
    git-svn-id: https://plugins.svn.wordpress.org/social-slider/trunk@630275 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- social-slider.php
+++ social-slider.php
@@ -3,13 +3,13 @@
 Plugin Name: Social Slider
 Plugin URI: http://xn--wicek-k0a.pl/projekty/social-slider
 Description: This plugin adds links to your social networking sites' profiles in a box floating at the left side of the screen.
-Version: 7.4.6
+Version: 7.4.7
 Author: Łukasz Więcek
 Author URI: http://majsterkowo.pl/
 */
 
 $socialslider			= "social-slider";
-$socialslider_wersja	= "7.4.6";
+$socialslider_wersja	= "7.4.7";
 $socialslider_baza		= str_replace("https://", "http://", get_bloginfo('wpurl'));
 $socialslider_katalog	= WP_PLUGIN_URL .'/'.$socialslider;
 
