ece753f0eafdad3ca0c10b5c1496852dca365ec8
LukaszWiecek <LukaszWiecek@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-04 13:30:03 +0000

    I added the user agent on BlackBerry browser
    git-svn-id: https://plugins.svn.wordpress.org/social-slider/trunk@633988 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- social-slider.php
+++ social-slider.php
@@ -3,13 +3,13 @@
 Plugin Name: Social Slider
 Plugin URI: http://xn--wicek-k0a.pl/projekty/social-slider
 Description: This plugin adds links to your social networking sites' profiles in a box floating at the left side of the screen.
-Version: 7.4.7
+Version: 7.4.8
 Author: Łukasz Więcek
 Author URI: http://majsterkowo.pl/
 */
 
 $socialslider			= "social-slider";
-$socialslider_wersja	= "7.4.7";
+$socialslider_wersja	= "7.4.8";
 $socialslider_baza		= str_replace("https://", "http://", get_bloginfo('wpurl'));
 $socialslider_katalog	= WP_PLUGIN_URL .'/'.$socialslider;
 
@@ -1243,6 +1243,7 @@ if(get_option('socialslider_mobile')=="nie" || !get_option('socialslider_mobile'
 		"blackberry9740",	// Torch2
         "blackberry9800",	// Torch
 		"blackberry9900",	// Torch
+		"blackberry9903",	// Torch3 
 		"webOS",			// Palm Pre Experimental
 		"incognito", 		// Other iPhone browser
 		"webmate", 			// Other iPhone browser
